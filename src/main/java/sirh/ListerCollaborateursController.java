3a96f9a86f8e0a85a74100d967f1dd20aceb77dc
duvaudierje <jerome.duvaudier@natixis.com>
2021-04-19 14:54:06 +0200

    Premier commit

---
+++ src/main/java/sirh/ListerCollaborateursController.java
@@ -0,0 +1,23 @@
+package sirh;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ListerCollaborateursController extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8733733130156558127L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("listeNoms", Arrays.asList("Robert", "Jean", "Hugues"));
+		req.getRequestDispatcher("/WEB-INF/views/collab/listerCollaborateurs.jsp").forward(req, resp);
+	}
+}
